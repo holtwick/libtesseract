7bee91c6fce940a7355f95a40df5348b83b2919b
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-11-23 14:44:28 +0100

    Local reference for debugging

--- Package.swift
+++ Package.swift
@@ -3,20 +3,23 @@
 import PackageDescription
 
 let package = Package(
-  name: "libtesseract",
-  products: [
-    .library(
-      name: "libtesseract",
-      targets: ["libtesseract"]
-    ),
-  ],
-  dependencies: [],
-  targets: [
-    .binaryTarget(
-      name: "libtesseract",
-      url: "https://dl.bintray.com/steven0351/tesseract/libtesseract-0.1.0.xcframework.zip",
-      checksum: "f732c6e1bbdbbdec87201841a4654d4d22e055e960ec618d48f5ec8141331af7"
-    )
-  ]
+    name: "libtesseract",
+    products: [
+        .library(
+            name: "libtesseract",
+            targets: ["libtesseract"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "libtesseract",
+            path: "./build/libtesseract.xcframework")
+        //        .binaryTarget(
+        //            name: "libtesseract",
+        //            url: "https://dl.bintray.com/steven0351/tesseract/libtesseract-0.1.0.xcframework.zip",
+        //            checksum: "f732c6e1bbdbbdec87201841a4654d4d22e055e960ec618d48f5ec8141331af7"
+        //        )
+    ]
 )
 
