0f75f3ae1034c0b8b97a3797e15baae34a6aa202
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-11-26 19:53:06 +0100

    Build leftovers

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "libtesseract",
+            type: .static,
             targets: ["libtesseract"]
         ),
     ],
@@ -15,11 +16,6 @@ let package = Package(
         .binaryTarget(
             name: "libtesseract",
             path: "./build/libtesseract.xcframework")
-        //        .binaryTarget(
-        //            name: "libtesseract",
-        //            url: "https://dl.bintray.com/steven0351/tesseract/libtesseract-0.1.0.xcframework.zip",
-        //            checksum: "f732c6e1bbdbbdec87201841a4654d4d22e055e960ec618d48f5ec8141331af7"
-        //        )
     ]
 )
 
